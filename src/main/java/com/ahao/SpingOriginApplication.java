c95b1bf22898b04afe0e255dc499db076e095498
ahao <798947941@qq.com>
2023-03-28 20:31:12 +0800

    Initial commit

---
+++ src/main/java/com/ahao/SpingOriginApplication.java
@@ -0,0 +1,13 @@
+package com.ahao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingOriginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingOriginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahao/SpingOriginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingOriginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
